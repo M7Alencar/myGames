61c538dad4ea4fcf2f4243fc644dc44a66c3e549
M7Alencar <matheusilva20rj@gmail.com>
2020-04-09 18:28:48 -0300

    v1
    Em desenvolvimento...

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+
+  // Configuração do Firebase --> Troque pela sua configuração pessoal
+  firebase: {
+  apiKey: "AIzaSyCGAe2OJ8RUVJSK-XKBNeeOXGSH26G-grg",
+    authDomain: "my-games-d52c8.firebaseapp.com",
+    databaseURL: "https://my-games-d52c8.firebaseio.com",
+    projectId: "my-games-d52c8",
+    storageBucket: "my-games-d52c8.appspot.com",
+    messagingSenderId: "851560242956",
+    appId: "1:851560242956:web:a5a80fac05577a6a3d7329"
+  }
+};
